3e0c2eec5eebd8659db8b6bcaad718509d9b7eba
Yaye Fatou Thiam <yayefatouthiam@Yayes-MacBook-Air.local>
2018-11-13 21:54:50 -0600

    push not working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ var config = {
     databaseURL: "https://train-scheduler-7211b.firebaseio.com",
     projectId: "train-scheduler-7211b",
     storageBucket: "",
-    messagingSenderId: "230081520462"
+    //messagingSenderId: "230081520462"
   };
 
   firebase.initializeApp(config);
@@ -17,29 +17,42 @@ var config = {
   var trainName = "";
   var destination = "";
   var frequency = 0;
+  var firstTrainTime = "";
 
-  //on click event for when the user click on teh button to add a new train
+  //on click event for when the user click on the button to add a new train
   $(".addTrain").on("click", function(){
+      event.preventDefault();
+
       trainName = $("#inputTrainName").val().trim();//get the value in the input boxes...
       destination = $("#inputDestination").val().trim();
       frequency = $("#inputFrequency").val().trim();
+      firstTrainTime = $("#inputFirstTrainTime").val().trim();
+      
+      console.log(trainName);
+      console.log(destination);
+      console.log(frequency);
+      console.log(firstTrainTime);
 
-      database.ref().push({//and insert them in out database
-          trainName = trainName,
-          destination = destination,
-          frequency = frequency,
+
+      database.ref().push({//and insert them in the database
+          trainName: trainName,
+          destination: destination,
+          frequency: frequency,
           dateAded: firebase.database.ServerValue.TIMESTAMP
       });
   });
+
 
   database.ref().on("child_added", function(childSnapshot){
     console.log(childSnapshot.val().trainName);
     console.log(childSnapshot.val().destination);
     console.log(childSnapshot.val().frequency);
+    console.log(childSnapshot.val().firstTrainTime);
 
   }, function(errorObject){
     console.log("Errors handled: " + errorObject.code);
   });
+
 
   database.ref().on("child_added",function(snapshot){
       var pushTrainName = snapshot.val().trainName;

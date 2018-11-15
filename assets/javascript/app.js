80831037af07218610773e438eefb971484d0b52
Yaye Fatou Thiam <yayefatouthiam@Yayes-MacBook-Air.local>
2018-11-14 19:45:57 -0600

    insert in Firebase working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,14 @@
-//Initialize Firebase
+
+// Initialize Firebase
 var config = {
     apiKey: "AIzaSyDmNWR2CtUS7D8XykI3s0RLf7r5_nKvFbw",
     authDomain: "train-scheduler-7211b.firebaseapp.com",
     databaseURL: "https://train-scheduler-7211b.firebaseio.com",
     projectId: "train-scheduler-7211b",
-    storageBucket: "",
-    //messagingSenderId: "230081520462"
-  };
-
-  firebase.initializeApp(config);
+    storageBucket: "train-scheduler-7211b.appspot.com",
+    messagingSenderId: "230081520462"
+   };
+   firebase.initializeApp(config);
 
   //Firebase database declaration
   var database = firebase.database();
@@ -19,8 +19,14 @@ var config = {
   var frequency = 0;
   var firstTrainTime = "";
 
+ //Global variables
+ //var trainName1  ;
+ //var destination1  ;
+ //var frequency1 ;
+ //var firstTrainTime1 ;
+
   //on click event for when the user click on the button to add a new train
-  $(".addTrain").on("click", function(){
+  $(".add-Train").on("click", function(){
       event.preventDefault();
 
       trainName = $("#inputTrainName").val().trim();//get the value in the input boxes...
@@ -28,7 +34,7 @@ var config = {
       frequency = $("#inputFrequency").val().trim();
       firstTrainTime = $("#inputFirstTrainTime").val().trim();
       
-      console.log(trainName);
+      console.log(trainName + "test");
       console.log(destination);
       console.log(frequency);
       console.log(firstTrainTime);
@@ -38,27 +44,50 @@ var config = {
           trainName: trainName,
           destination: destination,
           frequency: frequency,
-          dateAded: firebase.database.ServerValue.TIMESTAMP
+          firstTrainTime: firstTrainTime,
+          //dateAded: firebase.database.ServerValue.TIMESTAMP
       });
   });
 
 
   database.ref().on("child_added", function(childSnapshot){
+
+    var pushTrainName = childSnapshot.val().trainName;
+    var pushDestination = childSnapshot.val().destination;
+    var pushFrequency = childSnapshot.val().frequency;
+
     console.log(childSnapshot.val().trainName);
     console.log(childSnapshot.val().destination);
     console.log(childSnapshot.val().frequency);
     console.log(childSnapshot.val().firstTrainTime);
 
-  }, function(errorObject){
-    console.log("Errors handled: " + errorObject.code);
-  });
-
-
-  database.ref().on("child_added",function(snapshot){
-      var pushTrainName = snapshot.val().trainName;
-      var pushDestination = snapshot.val().destination;
-      var pushFrequency = snapshot.val().frequency;
-
     $(".train-table").append("<tr> <td>" + pushTrainName + "</td><td>" + pushDestination + "</td><td>" + pushFrequency + "</td></tr>");
+    empty();
+
+  }, function(errorObject){
+
+    console.log("Errors handled: " + errorObject.code);
+
   });
 
+  //function that empty the input boxes after insert
+  function empty(){
+      $("#inputTrainName").empty();
+      $("#inputDestination").empty();
+      $("#inputFirstTrainTime").empty();
+      $("#inputFrequency").empty();
+  }
+
+      
+
+   
+  
+   
+
+ 
+
+     
+
+
+
+   
